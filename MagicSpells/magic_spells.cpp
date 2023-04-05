21ad08f004f416095568dda8253e06988a9cb4cf
SeshHomeUbuntuPC <seshu.basava70@gmail.com>
2023-04-06 01:24:45 +0200

    updated MagicSpells

--- MagicSpells/magic_spells.cpp
+++ MagicSpells/magic_spells.cpp
@@ -77,19 +77,19 @@ void counterspell(Spell *spell) {
     which_spell->revealWaterpower();
 
   else {
-  auto name = spell->revealScrollName();
-        auto journal = SpellJournal::read();
+    auto name = spell->revealScrollName();
+    auto journal = SpellJournal::read();
 
-        vector<vector<size_t> > data(name.size() + 1, vector<size_t>(journal.size() + 1, 0));
-        for (size_t i = 0; i < name.size(); ++i) {
-            for (size_t j = 0; j < journal.size(); ++j) {
-                if (name[i] == journal[j])
-                    data[i+1][j+1] = data[i][j] + 1;
-                else
-                    data[i+1][j+1] = max(data[i][j + 1], data[i + 1][j]);
-            }
-        }
-        cout << data[name.size()][journal.size()] << endl;}
+    vector<vector<size_t> > data(name.size() + 1, vector<size_t>(journal.size() + 1, 0));
+    for (size_t i = 0; i < name.size(); ++i) {
+      for (size_t j = 0; j < journal.size(); ++j) {
+        if (name[i] == journal[j])
+          data[i+1][j+1] = data[i][j] + 1;
+        else
+          data[i+1][j+1] = max(data[i][j + 1], data[i + 1][j]);
+      }
+    }
+    cout << data[name.size()][journal.size()] << endl;}
 }
 
 class Wizard {
