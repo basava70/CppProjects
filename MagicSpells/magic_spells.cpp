846fccaf5172a478f4d14696019614d661fd28da
SeshHomeUbuntuPC <seshu.basava70@gmail.com>
2023-04-25 16:02:07 +0200

    Added functionality for negative numbers in spiral_matrix.

--- MagicSpells/magic_spells.cpp
+++ MagicSpells/magic_spells.cpp
@@ -1,128 +1,125 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 using namespace std;
 
-class Spell { 
-  private:
-    string scrollName;
-  public:
-    Spell(): scrollName("") { }
-    Spell(string name): scrollName(name) { }
-    virtual ~Spell() { }
-    string revealScrollName() {
-      return scrollName;
-    }
+class Spell {
+private:
+  string scrollName;
+
+public:
+  Spell() : scrollName("") {}
+  Spell(string name) : scrollName(name) {}
+  virtual ~Spell() {}
+  string revealScrollName() { return scrollName; }
 };
 
-class Fireball : public Spell { 
-  private: int power;
-  public:
-           Fireball(int power): power(power) { }
-           void revealFirepower(){
-             cout << "Fireball: " << power << endl;
-           }
+class Fireball : public Spell {
+private:
+  int power;
+
+public:
+  Fireball(int power) : power(power) {}
+  void revealFirepower() { cout << "Fireball: " << power << endl; }
 };
 
 class Frostbite : public Spell {
-  private: int power;
-  public:
-           Frostbite(int power): power(power) { }
-           void revealFrostpower(){
-             cout << "Frostbite: " << power << endl;
-           }
+private:
+  int power;
+
+public:
+  Frostbite(int power) : power(power) {}
+  void revealFrostpower() { cout << "Frostbite: " << power << endl; }
 };
 
-class Thunderstorm : public Spell { 
-  private: int power;
-  public:
-           Thunderstorm(int power): power(power) { }
-           void revealThunderpower(){
-             cout << "Thunderstorm: " << power << endl;
-           }
+class Thunderstorm : public Spell {
+private:
+  int power;
+
+public:
+  Thunderstorm(int power) : power(power) {}
+  void revealThunderpower() { cout << "Thunderstorm: " << power << endl; }
 };
 
-class Waterbolt : public Spell { 
-  private: int power;
-  public:
-           Waterbolt(int power): power(power) { }
-           void revealWaterpower(){
-             cout << "Waterbolt: " << power << endl;
-           }
+class Waterbolt : public Spell {
+private:
+  int power;
+
+public:
+  Waterbolt(int power) : power(power) {}
+  void revealWaterpower() { cout << "Waterbolt: " << power << endl; }
 };
 
 class SpellJournal {
-  public:
-    static string journal;
-    static string read() {
-      return journal;
-    }
-}; 
+public:
+  static string journal;
+  static string read() { return journal; }
+};
 string SpellJournal::journal = "";
 
 void counterspell(Spell *spell) {
   string spell_name = spell->revealScrollName();
   int count = 0;
 
-  if(Fireball *which_spell = dynamic_cast<Fireball*>(spell))
+  if (Fireball *which_spell = dynamic_cast<Fireball *>(spell))
     which_spell->revealFirepower();
 
-  else if(Frostbite *which_spell = dynamic_cast<Frostbite*>(spell))
+  else if (Frostbite *which_spell = dynamic_cast<Frostbite *>(spell))
     which_spell->revealFrostpower();
 
-  else if(Thunderstorm *which_spell = dynamic_cast<Thunderstorm*>(spell))
+  else if (Thunderstorm *which_spell = dynamic_cast<Thunderstorm *>(spell))
     which_spell->revealThunderpower();
 
-  else if(Waterbolt *which_spell = dynamic_cast<Waterbolt*>(spell))
+  else if (Waterbolt *which_spell = dynamic_cast<Waterbolt *>(spell))
     which_spell->revealWaterpower();
 
   else {
     auto name = spell->revealScrollName();
     auto journal = SpellJournal::read();
 
-    vector<vector<size_t> > data(name.size() + 1, vector<size_t>(journal.size() + 1, 0));
+    vector<vector<size_t>> data(name.size() + 1,
+                                vector<size_t>(journal.size() + 1, 0));
     for (size_t i = 0; i < name.size(); ++i) {
       for (size_t j = 0; j < journal.size(); ++j) {
         if (name[i] == journal[j])
-          data[i+1][j+1] = data[i][j] + 1;
+          data[i + 1][j + 1] = data[i][j] + 1;
         else
-          data[i+1][j+1] = max(data[i][j + 1], data[i + 1][j]);
+          data[i + 1][j + 1] = max(data[i][j + 1], data[i + 1][j]);
       }
     }
-    cout << data[name.size()][journal.size()] << endl;}
+    cout << data[name.size()][journal.size()] << endl;
+  }
 }
 
 class Wizard {
-  public:
-    Spell *cast() {
-      Spell *spell;
-      string s; cin >> s;
-      int power; cin >> power;
-      if(s == "fire") {
-        spell = new Fireball(power);
-      }
-      else if(s == "frost") {
-        spell = new Frostbite(power);
-      }
-      else if(s == "water") {
-        spell = new Waterbolt(power);
-      }
-      else if(s == "thunder") {
-        spell = new Thunderstorm(power);
-      } 
-      else {
-        spell = new Spell(s);
-        cin >> SpellJournal::journal;
-      }
-      return spell;
+public:
+  Spell *cast() {
+    Spell *spell;
+    string s;
+    cin >> s;
+    int power;
+    cin >> power;
+    if (s == "fire") {
+      spell = new Fireball(power);
+    } else if (s == "frost") {
+      spell = new Frostbite(power);
+    } else if (s == "water") {
+      spell = new Waterbolt(power);
+    } else if (s == "thunder") {
+      spell = new Thunderstorm(power);
+    } else {
+      spell = new Spell(s);
+      cin >> SpellJournal::journal;
     }
+    return spell;
+  }
 };
 
 int main() {
   int T;
   cin >> T;
   Wizard Arawn;
-  while(T--) {
+  while (T--) {
     Spell *spell = Arawn.cast();
     counterspell(spell);
   }
